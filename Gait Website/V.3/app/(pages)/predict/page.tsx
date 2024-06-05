6aae8d0d44b8d1a7d3966ed09358666e10d48651
Lance Aziz <115536960+LanceAziz@users.noreply.github.com>
2024-06-05 15:06:24 +0300

    changes

--- Gait Website/V.3/app/(pages)/predict/page.tsx
+++ Gait Website/V.3/app/(pages)/predict/page.tsx
@@ -1,25 +1,24 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faServer } from '@fortawesome/free-solid-svg-icons';
+'use client';
 import styles from "./Predict.module.css"
 import Link from "next/link";
 
-
-
 function Predict() {
-    const inputOptions = [
-        { name: 'Live' },
-        { name: 'Browse' }
-    ]
+    let browseElement = document.getElementById('browseElement')
+    function openCamera() {
+        browseElement?.classList.add('d-none');
+    }
+    function openFileBrowser() {
+        console.log("browser opened");
+        browseElement?.classList.remove('d-none');
+    }
 
     function mediaSource(source: string) {
         switch (source) {
             case source = 'Live':
-                // openCamera();
-                console.log("camera opened")
+                openCamera();
                 break;
             case source = 'Browse':
-                // openFileBrowser();
-                console.log("browser opened")
+                openFileBrowser();
                 break;
             default:
                 console.log("Unknown Source selected");
@@ -32,19 +31,33 @@ function Predict() {
                     <div className="col-12 d-flex justify-content-center  py-3">
                         <div className={`${styles.radioInputs}`}>
                             <label className={`${styles.radio}`}>
-                                <input type="radio" name="radio" />
-                                <span className={`${styles.name} fs-5`}>Live</span>
+                                <input id="liveInput" type="radio" name="radio" />
+                                <span onClick={() => mediaSource('Live')} className={`${styles.name} fs-5`}>Live</span>
                             </label>
                             <label className={`${styles.radio}`}>
-                                <input type="radio" name="radio" />
-                                <span className={`${styles.name} fs-5`}>Browse</span>
+                                <input id="BrowseInput" type="radio" name="radio" />
+                                <span onClick={() => mediaSource('Browse')} className={`${styles.name} fs-5`}>Browse</span>
                             </label>
                         </div>
+                    </div>
+                    <div id="browseElement" className="col-12 d-flex justify-content-center pb-3 d-none">
+                        <div className="col-8 bg-black rounded rounded-5">d</div>
                     </div>
                 </div>
                 <div className="d-flex justify-content-center align-items-center">
                     <div className="z-2 position-absolute">
-                        <button className={`${styles.gaitRounded}`}>Start</button>
+                        <button id="startButton" className={`${styles.gaitRounded}`}>Start</button>
+                        <button id="loader" className={`${styles.hourglassBackground} d-none`}>
+                            <div className={`${styles.hourglassContainer}`}>
+                                <div className={`${styles.hourglassCurves}`}></div>
+                                <div className={`${styles.hourglassCapTop}`}></div>
+                                <div className={`${styles.hourglassGlassTop}`}></div>
+                                <div className={`${styles.hourglassSand}`}></div>
+                                <div className={`${styles.hourglassSandStream}`}></div>
+                                <div className={`${styles.hourglassCapBottom}`}></div>
+                                <div className={`${styles.hourglassGlass}`}></div>
+                            </div>
+                        </button>
                     </div>
                     <div className={`col-md-12 ${styles.videoPlaceholder} `}></div>
                 </div>
